69c0f6cec278f66fd7e601c19d97b8593fd2d1d7
Spencer DeLange <spencer.delange@gmail.com>
2022-09-10 12:30:25 -0700

    Add instrumentation test and change formatting

--- FitnessApp/app/src/androidTest/java/com/example/fitnessapp/ExampleInstrumentedTest.kt
+++ FitnessApp/app/src/androidTest/java/com/example/fitnessapp/ExampleInstrumentedTest.kt
@@ -1,12 +1,23 @@
 package com.example.fitnessapp
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.content.Intent
+import android.provider.*
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.*
 
-import org.junit.Assert.*
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +26,19 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.fitnessapp", appContext.packageName)
     }
+
+
+    @Test
+    fun testImageClick() {
+        onView(withId(R.id.pic_button))
+            .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+    }
+
 }
