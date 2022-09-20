7ff901a62f9e721411c21de36631bd108e3742bc
Spencer DeLange <spencer.delange@gmail.com>
2022-09-19 19:37:36 -0600

    add weather to navbar

--- FitnessApp/app/src/main/java/com/example/fitnessapp/HikesActivity.kt
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/HikesActivity.kt
@@ -14,6 +14,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class HikesActivity : AppCompatActivity() {
     lateinit var bottomNav : BottomNavigationView
     private var homeIntent: Intent? = null
+    private var weatherIntent: Intent? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hikes)
@@ -24,11 +25,20 @@ class HikesActivity : AppCompatActivity() {
         val receivedIntent = intent
         var user = receivedIntent.extras?.getSerializable("user") as User
         val imagePath = receivedIntent.getStringExtra("imagePath")
+        val userCity = receivedIntent.getStringExtra("the_city")
+        val userCountry = receivedIntent.getStringExtra("the_country")
 
         homeIntent = Intent(this, HomeActivity::class.java)
         homeIntent!!.putExtra("user", user)
         homeIntent!!.putExtra("imagePath", imagePath)
-        Log.d("in ", "hikesactivity")
+        homeIntent!!.putExtra("the_city", userCity)
+        homeIntent!!.putExtra("the_country", userCountry)
+
+        weatherIntent = Intent(this, weatherActivity::class.java)
+        weatherIntent!!.putExtra("user", user)
+        weatherIntent!!.putExtra("imagePath", imagePath)
+        weatherIntent!!.putExtra("the_city", userCity)
+        weatherIntent!!.putExtra("the_country", userCountry)
 
         bottomNav.setOnItemSelectedListener {
             Log.d("it.itemId: ", it.itemId.toString())
@@ -38,6 +48,10 @@ class HikesActivity : AppCompatActivity() {
                     return@setOnItemSelectedListener true
                 }
                 R.id.hikes -> {
+                    return@setOnItemSelectedListener true
+                }
+                R.id.weather -> {
+                    startActivity(weatherIntent)
                     return@setOnItemSelectedListener true
                 }
                 else -> {
