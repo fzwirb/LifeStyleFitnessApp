d79da2173cf393729782269613f465f59ed7d0a4
spencerdelange <spencer.delange@gmail.com>
2022-10-26 17:47:27 -0600

    initial commit

---
+++ FitnessApp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "fitnessapp2355ece1": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "fitnessapp": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
